8bc5c766b5277c7d2952fba087d829cfa2e3a120
liuhui <liuhui@ihomefnt.com>
2020-05-07 17:49:40 +0800

    COMMON_SWAGGER_DES

--- src/constants/common.constant.ts
+++ src/constants/common.constant.ts
@@ -1,5 +1,7 @@
 export const COMMON_SECRETKEY = '1AGy4bCUoECDZ4yI6h8DxHDwgj84EqStMNyab8nPChQ';
 export const COMMON_DEFAULT_SGY = 'jwt';
 
+export const COMMON_SWAGGER_DES = `数据格式: { code: number, data: T, success: boolean, message: string } code为0时，请求成功，code为1时，请求异常，T为response中的schema`;
+
 export const LOG4JS_OPTION = Symbol("LOG4JS_OPTION");
 export const CONFIG_OPTION = Symbol("CONFIG_OPTION");
